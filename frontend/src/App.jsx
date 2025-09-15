017ac675c2263b7213162e046a510c89e098a08c
monu siddiki <m9800564411@gmail.com>
2025-09-15 23:41:28 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PlantShop from "./components/PlantShop";
 import ElectronicsShop from "./components/ElectronicsShop";
 import CosmeticsShop from "./components/CosmeticsShop";
+import TeaCoffeeShop from "./components/TeaCoffeeShop";
+import ShoesShop from "./components/ShoesShop";
+import EarringsShop from "./components/EarringsShop";
 import Home from "./components/Home";
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
           <Route path="/plant-shop" element={<PlantShop />} />
           <Route path="/electronics-shop" element={<ElectronicsShop />} />
           <Route path="/cosmetics-shop" element={<CosmeticsShop />} />
+          <Route path="/tea-coffee-shop" element={<TeaCoffeeShop />} />
+          <Route path="/shoes-shop" element={<ShoesShop />} />
+          <Route path="/earrings-shop" element={<EarringsShop />} />
         </Routes>
       </BrowserRouter>
     </div>
